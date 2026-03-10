7d6bbc547d23dd93e78c0f16ce3349b4d873c3ed
Moses Avila <mosesavila9@gmail.com>
2026-03-09 17:01:33 -0700

    added index, closes #23

--- create-indexes.sql
+++ create-indexes.sql
@@ -1,2 +1,5 @@
--- index on customer last name 
+-- index on Customer last name 
 CREATE INDEX idx_customer_lname ON Customer(lname);
+
+-- index on Car customer_id so we can find all cars from a certain customer faster
+CREATE INDEX idx_car_customer_id ON Car(customer_id);
