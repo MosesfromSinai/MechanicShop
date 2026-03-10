1e93898f2597cb37714cc726eef9ce0efd215f47
Moses Avila <mosesavila9@gmail.com>
2026-03-09 17:13:09 -0700

    closes #26

--- create-indexes.sql
+++ create-indexes.sql
@@ -6,3 +6,9 @@ CREATE INDEX idx_car_customer_id ON Car(customer_id);
 
 -- index on Service_Request car_vin to find service reqs for cars
 CREATE INDEX idx_sr_car_vin ON Service_Request(car_vin);
+
+-- index on Service_Request customer_id for total bill query
+CREATE INDEX idx_sr_customer_id ON Service_Request(customer_id);
+
+-- index on Service_Request close_date to find open vs close date reqs
+CREATE INDEX idx_sr_close_date ON Service_Request(close_date);
