1adcc94fb5f736b344030478411b108aa1f30224
Moses Avila <mosesavila9@gmail.com>
2026-03-09 17:10:12 -0700

    closes #24

--- create-indexes.sql
+++ create-indexes.sql
@@ -3,3 +3,6 @@ CREATE INDEX idx_customer_lname ON Customer(lname);
 
 -- index on Car customer_id so we can find all cars from a certain customer faster
 CREATE INDEX idx_car_customer_id ON Car(customer_id);
+
+-- index on Service_Request car_vin to find service reqs for cars
+CREATE INDEX idx_sr_car_vin ON Service_Request(car_vin);
